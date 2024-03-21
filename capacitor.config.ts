16c0d3092c804eb82f6459e79fe5cde3071d3f15
CBPaulS <paulchacon13@gmail.com>
2024-03-21 16:43:01 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'CBP_Termometr',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
